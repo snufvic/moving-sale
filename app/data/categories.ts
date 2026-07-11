5a1864603590ee465534bcb67ef70e073cbb6b15
snufvic <iamitw80@gmail.com>
2026-07-11 16:37:10 +0300

    add camping cat

--- app/data/categories.ts
+++ app/data/categories.ts
@@ -1,15 +1,16 @@
 export const CATEGORIES = [
-  "ריהוט",
   "מוצרי חשמל",
+  "קמפינג",
   "מטבח",
   "מוזיקה",
   "ספרים",
   "ילדים",
   "גינה",
+  "ריהוט",
   "כלי עבודה",
   "אחר",
 ] as const;
 
 export type Category = (typeof CATEGORIES)[number];
 
-export const DEFAULT_CATEGORY: Category = "ריהוט";
+export const DEFAULT_CATEGORY: Category = "מוצרי חשמל";
